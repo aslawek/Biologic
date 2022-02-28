fa569661f6c37f1e672dbed4f34a4dd94d4fff83
Andrzej Sławek <aslawek1990@gmail.com>
2022-02-28 13:51:28 +0100

    SRDP scripts for Tomek finished

--- README.md
+++ README.md
@@ -8,15 +8,21 @@ load_data.py – loads data either from file (give path with „/”) or from pa
 
 save_data.py – saves data from given global variable (pandas dataframe) as a file (for some reason project needs some time to refresh)
 
-assign_states_and_cycles.py – takes „data” dataframe (global var) and assigns state and cycle according to sequence. In practice it checks the change when control/V changes, then loops state assignment
-
-assign_cycle_CV.py - takes "data" dataframe (global var) and assigns cycles according to the sign change of the control/V value (every 2 changes of sign, cycle += 1).
-
-extract_on_off.py – for each cycle take all read_set and read_reset points and calculate median(). Returns data_on_off global variable that can be saved as a file
+filter_acc_to_ctrlV.py - takes the given data (global var) and filters it according to the given value of control/V (+/- standard 0.01 V) 
 
 cut/rm_data_by_plot.py – cuts/removes part of data according to clickable plot
 
 cut/rm_data_by_time_range.py – cuts/removes part of data according to given ranges
+
+assign_states_and_cycles.py – takes „data” dataframe (global var) and assigns state and cycle according to sequence. In practice it checks the change when control/V changes, then loops state assignment
+
+assign_cycle_CV.py - takes "data" dataframe (global var) and assigns cycles according to the sign change of the control/V value (every 2 changes of sign, cycle += 1).
+
+assign_time_reads.py - takes "data_filtered" (global var) and calculate the "standard" time/s change (i.e. median of change in time/s between consecutive datapoints). Adds cycle column. Increases the cycle number if the next point is 1.1 times greater than "standard" time/s change.
+
+extract_on_off.py – for each cycle take all read_set and read_reset points and calculate median(). Returns data_on_off global variable that can be saved as a file
+
+extract_time_reads.py - for each cycle in "data_filtered" (global var) calculate median for I/mA. Split them for I_odd/mA for cycles 1,3,5... and I_even for cycles 2,4,6...
 
 plot_time_ctrlV_I.py - plots either global variable (pandas dataframe) or data from file. Ewe/V and I/mA is a function of time/s
 
--- extract_time_reads.py
+++ extract_time_reads.py
@@ -14,30 +14,21 @@ main = lambda f: f(data_filtered)
 def main(data_filtered):
     global data_time_reads
 
-    print('Hi! I will extract time reads data for each cycle using median() for \n')
+    print('Hi! For data_filtered global variable I will calculate median() for I/mA for each cycle and split the values for odd (1,3,5..) and even (2,4,6...) integers.\n')
 
-    data_time_reads = pd.DataFrame({'cycle': pd.Series(dtype='int'), 'I_odd/mA': pd.Series(dtype='float64'), 'I_even/mA': pd.Series(dtype='float64')})
-    for cycle in range(data_filtered['cycle'].max()):
-        print(f'Working on cycle {cycle} out of  {data_filtered["cycle"].max()}...')
+    data_time_reads = pd.DataFrame({'cycle': pd.Series(dtype='int32'), 'I_odd/mA': pd.Series(dtype='float64'), 'I_even/mA': pd.Series(dtype='float64')})
 
-        #row = pd.Series({
-        #    'cycle': np.ceil((cycle + 1) / 2),
-        #    'I_odd/mA' : data_filtered.loc[(data_filtered['cycle'] == cycle + 1) & (cycle % 2 == 0)]['I/mA'].median(),
-        #    'I_even/mA': data_filtered.loc[(data_filtered['cycle'] == cycle + 1) & (cycle % 2 == 1)]['I/mA'].median()})
+    for cycle in range(int(data_filtered['cycle'].max() / 2)):
+        print(f'Working on cycle {cycle} out of {int(data_filtered["cycle"].max() / 2)}:\n\tI_odd/mA  is taken from data_filtered cycle: {cycle * 2 + 1}\n\tI_even/mA is taken from data_filtered cycle: {cycle * 2 + 2}')
 
-        #data_time_reads = data_time_reads.append(row, index=cycle)
+        data_time_reads = data_time_reads.append({
+            'cycle': cycle + 1,
+            'I_odd/mA': data_filtered.loc[data_filtered['cycle'] == (cycle * 2 + 1)]['I/mA'].median(),
+            'I_even/mA': data_filtered.loc[data_filtered['cycle'] == (cycle * 2 + 2)]['I/mA'].median()}, ignore_index=True)
 
-        data_time_reads = data_time_reads.append(pd.Series({
-            'cycle': np.ceil((cycle + 1) / 2),
-            'I_odd/mA' : data_filtered.loc[(data_filtered['cycle'] == cycle + 1) & (cycle % 2 == 0)]['I/mA'].median(),
-            'I_even/mA': data_filtered.loc[(data_filtered['cycle'] == cycle + 1) & (cycle % 2 == 1)]['I/mA'].median()}, name=int(np.ceil((cycle + 1) / 2))))
+    data_time_reads['cycle'] = data_time_reads['cycle'].astype('int32')
 
-        #data_time_reads = data_time_reads.append({
-        #    'cycle': np.ceil((cycle + 1) / 2),
-        #    'I_odd/mA' : data_filtered.loc[(data_filtered['cycle'] == cycle + 1) & (cycle % 2 == 0)]['I/mA'].median(),
-        #    'I_even/mA': data_filtered.loc[(data_filtered['cycle'] == cycle + 1) & (cycle % 2 == 1)]['I/mA'].median()}, ignore_index=True)
-
-    print(f'\n{data_time_reads}')
-    #return data_time_reads
+    print(f'\nYour output "data_time_reads":\n{data_time_reads}')
+    return data_time_reads
 
 del(main)
