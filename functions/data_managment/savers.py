Added bsl_corr parameter. Autolab's "zero" current is below zero and causes some wrong results when reading with very low potentials.

functions/data_managment/savers.py
@@ -1,6 +1,8 @@
 import os
 
 import numpy as np
+import pandas as pd
+
 
 def saver_data(data, filename):
     prefix = 'data_'
@@ -43,10 +45,17 @@ def saver_data_STDP(data, filename, dt):
     folder_name = filename.replace("/", " ").split()[0] + "/data_STDP_" + "/"
     os.makedirs(folder_name, exist_ok=True)
 
-    prefix = f'data_STDP_{np.round(dt, 5)}_s_'
+#    prefix = f'data_STDP_{np.round(dt, 5)}_s_'
+    prefix = f'data_STDP_' #alternative no dt in the filename
     output_filename =  folder_name + f'{prefix}{filename.replace("/", " ").split()[1]}'
     print(f'\n Saving data as {output_filename}')
-    data.to_csv(f'{output_filename}', sep='\t', index=False)
+
+    defined_dt = f'{np.round(dt, 5)}_s_'
+    defined_dt_df = pd.DataFrame({'defined_dt': [defined_dt]})
+
+    data = pd.concat([data, defined_dt_df])
+
+    data.to_csv(f'{output_filename}', sep='\t', index=False,)
 
 def saver_STDP(data, filename):
     full_name = '00_STDP_summary.txt'